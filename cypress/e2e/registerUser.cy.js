0f3b370043f63c6684473b4a9ecad4af33fca724
matraklazar <lazar.matrak1@gmail.com>
2023-06-05 19:10:03 +0200

    add registerUser.js and registerUser.cy.js

---
+++ cypress/e2e/registerUser.cy.js
@@ -0,0 +1,15 @@
+/// <reference types = "cypress" />
+import RegisterUser from "../pom/registerUser";
+
+const regUser = new RegisterUser();
+describe('Register user Tests',()=>{
+    it('Landed on Register page',()=>{
+        cy.visit('register');
+    });
+
+    it('Successful user registration',()=>{
+        regUser.register();
+        cy.url().should('eql',Cypress.config('baseUrl'));
+        cy.get('h1').should('contain','All Galleries');
+    });
+})
---
+++ cypress/pom/registerUser.js
@@ -0,0 +1,22 @@
+import{faker} from '@faker-js/faker'
+
+class RegisterUser{
+    _firstName = faker.person.firstName();
+    _lastName = faker.person.lastName();
+    _email = faker.internet.email({firstName: this._firstName, lastName: this._lastName});
+    _password = faker.internet.password();
+
+
+    register(){
+        cy.visit('register');
+        cy.get('#first-name').type(this._firstName);
+        cy.get('#last-name').type(this._lastName);
+        cy.get('#email').type(this._email);
+        cy.get('#password').type(this._password);
+        cy.get('#password-confirmation').type(this._password)
+        cy.get('.form-check-input').check();
+        cy.get('.btn').click();
+    }
+}
+
+export default RegisterUser;
